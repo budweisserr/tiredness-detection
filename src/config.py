a63919a542e51069663f26b2542e76b3739b4b5f
Denys <denys@mail.com>
2025-05-14 22:54:35 +0300

    just config with paths

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Application paths and constants
+APP_CONFIG = {
+    'predictor_path': '../predictor/shape_predictor_68_face_landmarks.dat',
+    'left_eye_path': '../image/left-eye.jpg',
+    'right_eye_path': '../image/right-eye.jpg',
+    'alert_sound': '../sound/alert.mp3',
+    'focus_sound': '../sound/focus.mp3',
+    'break_sound': '../sound/break.mp3',
+}
